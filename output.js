b3e1b99827459db41660152acc15154b4df412eb
action <action@github.com>
2024-07-06 10:08:28 +0000

    Add decoded output file

--- output.js
+++ output.js
@@ -1,4 +1,4 @@
-//Fri Jul 05 2024 22:27:46 GMT+0000 (Coordinated Universal Time)
+//Sat Jul 06 2024 10:08:28 GMT+0000 (Coordinated Universal Time)
 //Base:https://github.com/echo094/decode-js
 //Modify:https://github.com/smallfawn/decode_action
 const axios = require("axios");
